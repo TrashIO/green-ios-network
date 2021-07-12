1e5a99e6dda91936cac1c67d73a6b3808465c497
Abhishek Kumar Ravi <abhishek.ravi@oyorooms.com>
2021-07-13 03:11:44 +0530

    Added List Testcase

---
+++ ios-network-module/ios-network-module-tests/ListTests.swift
@@ -0,0 +1,24 @@
+//
+//  ListTests.swift
+//  ios-network-module-tests
+//
+//  Created by Abhishek on 13/07/21.
+//
+
+import XCTest
+
+class ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+
+}
